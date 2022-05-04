c02b19f881f38944cfc84e14b995ad47b3854a6b
dewzye2 <prankbooh1@gmail.com>
2022-05-04 14:39:36 -0300

    test 01

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css'
 import Navbar from './components/Navbar'
@@ -11,22 +11,31 @@ import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
 import ForgotPassword from './pages/ForgotPassword'
 import CreateListing from './pages/CreateListing'
+import EditListing from './pages/EditListing'
+import Listing from './pages/Listing'
+import Contact from './pages/Contact'
 
 function App() {
   return (
     <>
       <Router>
         <Routes>
-          <Route path='/' element={<Explore />}/>
-          <Route path='/offers' element={<Offers />}/>
-          <Route path='/category/:categoryName' element={<Category />}/>
+          <Route path='/' element={<Explore />} />
+          <Route path='/offers' element={<Offers />} />
+          <Route path='/category/:categoryName' element={<Category />} />
           <Route path='/profile' element={<PrivateRoute />}>
             <Route path='/profile' element={<Profile />} />
           </Route>
-          <Route path='/sign-in' element={<SignIn />}/>
-          <Route path='/sign-up' element={<SignUp />}/>
-          <Route path='/forgot-password' element={<ForgotPassword />}/>
-          <Route path='/create-listing' element={<CreateListing />}/>
+          <Route path='/sign-in' element={<SignIn />} />
+          <Route path='/sign-up' element={<SignUp />} />
+          <Route path='/forgot-password' element={<ForgotPassword />} />
+          <Route path='/create-listing' element={<CreateListing />} />
+          <Route path='/edit-listing/:listingId' element={<EditListing />} />
+          <Route
+            path='/category/:categoryName/:listingId'
+            element={<Listing />}
+          />
+          <Route path='/contact/:landlordId' element={<Contact />} />
         </Routes>
         <Navbar />
       </Router>
--- src/hooks/useAuthStatus.js
+++ src/hooks/useAuthStatus.js
@@ -7,22 +7,26 @@ export const useAuthStatus = () => {
   const isMounted = useRef(true)
 
   useEffect(() => {
-    if(isMounted) {
+    if (isMounted) {
       const auth = getAuth()
-        onAuthStateChanged(auth, (user) => {
-            if(user) {
-              setLoggedIn(true)
-            }
-            setCheckingStatus(false)
-        })
-    } 
-    
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+          setLoggedIn(true)
+        }
+        setCheckingStatus(false)
+      })
+    }
+
     return () => {
       isMounted.current = false
     }
   }, [isMounted])
 
-  return {loggedIn, checkingStatus}
+  return { loggedIn, checkingStatus }
 }
 
-export default useAuthStatus
+// Protected routes in v6
+// https://stackoverflow.com/questions/65505665/protected-route-with-firebase
+
+// Fix memory leak warning
+// https://stackoverflow.com/questions/59780268/cleanup-memory-leaks-on-an-unmounted-component-in-react-hooks
